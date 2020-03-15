90197dd262f577a0c0c81c2097e798e77d618e3c
Kartikeya Gokhale <kgkartikeyagokhale@gmail.com>
2020-03-15 23:01:09 +0530

    Added summarize

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,7 @@
 console.log("frontend running");
 const baseUrl = "http://localhost:9828";
 const RandImgBtn = document.getElementById("RandomIButton");
+const txtSummarize = document.getElementById("txtSummarize");
 
 const featureForm = $("#featureForm");
 
@@ -29,23 +30,6 @@ RandImgBtn.addEventListener("click", e => {
           </rect>
         </svg>
       </div>`);
-	// $.ajax({
-	// 	url: `${baseUrl}/random-image`,
-	// 	headers: { "Access-Control-Allow-Origin": `${baseUrl}/random-image` },
-	// 	type: "GET",
-	// 	crossDomain: true,
-
-	// 	success: function(fileName) {
-	// 		if (fileName) {
-	// 			outputBox.html(
-	// 				`<img class="card-img-top random-image" src="${baseUrl}/dcgan/resultImg/${fileName}">`
-	// 			);
-	// 		}
-	// 	},
-	// 	error: function(xhr, status) {
-	// 		alert("error");
-	// 	}
-	// });
 	setTimeout(() => {
 		outputBox.html(
 			`<h2> The Compressed Video </h2>
@@ -58,5 +42,31 @@ RandImgBtn.addEventListener("click", e => {
 			</video>
 			`
 		);
+	}, 1000);
+});
+
+txtSummarize.addEventListener("click", e => {
+	e.preventDefault();
+
+	outputBox.html(`<div class="loader loader--style5 text-center" title="4">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px" y="0px" width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;"
+          xml:space="preserve">
+          <rect x="0" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="10" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.2s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+          <rect x="20" y="0" width="4" height="10" fill="#333">
+            <animateTransform attributeType="xml" attributeName="transform" type="translate" values="0 0; 0 20; 0 0"
+              begin="0.4s" dur="0.6s" repeatCount="indefinite" />
+          </rect>
+        </svg>
+      </div>`);
+	setTimeout(() => {
+		outputBox.html(`<p>Lorem Ipsum Dolor</p>`);
 	}, 1000);
 });
